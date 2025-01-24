6b63204cb627d711d2f4a56e262d53db0562ffcd
JustAMob <carloencillo@gmail.com>
2025-01-24 10:50:57 +0800

    First Commit

--- blog_posts/urls.py
+++ blog_posts/urls.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.urls import path
 from . import views
-
+from .views import UserListCreate, PostListCreate, CommentListCreate
 urlpatterns = [
     path('', lambda request: HttpResponse("Welcome to the Posts Home Page!"), name='posts_home'),  # Default view
-    path('users/', views.get_users, name='get_users'),
-    path('users/create/', views.create_user, name='create_user'),
-    path('posts/', views.get_posts, name='get_posts'),
-    path('posts/create/', views.create_post, name='create_post'),
+    path('users/', UserListCreate.as_view(), name='user-list-create'),
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+    path('users/create/', views.update_user, name='update_user'),
+    path('users/', views.delete_user, name='delete_users'),
 ]
